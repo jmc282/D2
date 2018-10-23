63e9a8815d1078e10601ba635048305fa6f1d591
jmc282 <jmc282@pitt.edu>
2018-10-23 17:19:31 -0400

    tests

---
+++ game_test.rb
@@ -0,0 +1,25 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+
+require_relative 'game'
+
+class GameTest < Minitest::Test
+
+	def test_random_int
+	end
+
+	def test_display_starting_message
+	end
+
+	def test_get_units
+	end
+
+	def test_convert_currency
+	end
+
+	def test_stop_search?
+	end
+
+end
