5c4c526025d47e63c4c49db2571e0cc51fcd9f6d
jmc282 <jmc282@pitt.edu>
2018-10-22 23:46:25 -0400

    10/22/18

--- game.rb
+++ game.rb
@@ -2,8 +2,11 @@ require_relative 'location.rb'
 require_relative 'player.rb'
 
 # This is the game class for the gold_rush game
+
 class Game < Location
+
   # Game class constants
+
   SUTTER_CREEK = Location.new 'Sutter Creek', 0, 2, 2
   COLOMA = Location.new 'Coloma', 0, 3, 2
   ANGELS_CAMP = Location.new "Angel's Camp", 0, 4, 3
@@ -13,6 +16,7 @@ class Game < Location
   EL_DORADO = Location.new 'El Dorado Canyon', 10, 0, 4
 
   # Set neighbor locations of each location
+
   SUTTER_CREEK.set_neighbors COLOMA, ANGELS_CAMP, nil, nil
   COLOMA.set_neighbors SUTTER_CREEK, VIRGINIA_CITY, nil, nil
   ANGELS_CAMP.set_neighbors SUTTER_CREEK, NEVADA_CITY, VIRGINIA_CITY, nil
@@ -24,6 +28,7 @@ class Game < Location
   PLAYER = Player.new SUTTER_CREEK, 1, 0, 0
 
   # Initialization of game
+
   def initialize seed, player
     @seed = seed                # seed value for the game
     @which_player = player 		  # which player is playing (i.e Prospector 1)
@@ -31,25 +36,29 @@ class Game < Location
   end
 
   # Generates a pseudorandom integer between given max and min values
+
   def random_int min, max
     return @prng.rand(min..max)
-  end
+  end 
 
   def display_starting_message
     puts "Prospector #{@which_player} starting in #{PLAYER.current_location.name}."
   end
 
   def display_location_message last_location
-    puts "Heading from #{last_location} to #{PLAYER.current_location.name}."
+    print "Heading from #{last_location} to #{PLAYER.current_location.name}, "
+    puts "holding #{PLAYER.gold} gold and #{PLAYER.silver} silver"
   end
   
   # Finds which location the miner heads to next given the current location
+
   def next_location location
     num = random_int(0, location.amt_neighbors-1) 
     return location.neighbors[num]
   end
 
   # Returns the amount of gold and silver found in one iteration at a given location 
+
   def prospect location
     gold = random_int(0, location.max_gold)
     silver = random_int(0, location.max_silver)
@@ -57,6 +66,7 @@ class Game < Location
   end 
   
   # Returns false if the miner finds no silver and no gold at a location. Returns true otherwise. 
+
   def continue_search? silver, gold
     if silver == 0 && gold == 0 
       return false 
@@ -67,8 +77,10 @@ class Game < Location
 
   # Search for gold in a given location. Prospect at the location until the miner finds 
   # less than the minimum gold and silver. 
+
   def search location
     gold_found, silver_found = prospect(location)
+    save(silver_found, gold_found)
     if silver_found == 0 and gold_found == 0
       display_no_metals_found(location.name)
     else   
@@ -82,6 +94,7 @@ class Game < Location
   end
 
   # Calls a method to  display the amounts of silver and gold found at a location in proper units.
+
   def display_metals_found silver, gold, location
     display_metal(silver, "silver", location)
     display_metal(gold, "gold", location)
@@ -95,11 +108,17 @@ class Game < Location
     end
   end
 
+  def save silver_found, gold_found
+    PLAYER.add_silver(silver_found)
+    PLAYER.add_gold(gold_found)
+  end
+
   # During the first three locations a prospector searches, they shall leave a location 
   # if they find no silver and no gold. If they find any silver or gold, 
   # they will stay at the location for another iteration.
 
   # Play the game
+
   def play
     display_starting_message
     5.times do 
--- player.rb
+++ player.rb
@@ -1,11 +1,12 @@
 class Player
 
   # Initialization of Player
-  def initialize(current_location, visits, silver, gold)
+  def initialize current_location, visits, silver, gold
 	@current_location = current_location
 	@visits = visits
 	@silver = silver
 	@gold = gold
+  @days = 0
   end
 
   def current_location
@@ -24,8 +25,24 @@ class Player
   	@gold
   end
 
+  def days
+    @days
+  end
+
+  def add_day
+    @days += 1
+  end
+
+  def add_silver silver_found
+    @silver += silver_found
+  end 
+
+  def add_gold gold_found
+    @gold += gold_found
+  end
+
   # Sets the location of the gold miner
-  def set_location(location)
+  def set_location location
   	@current_location = location
   end
 
