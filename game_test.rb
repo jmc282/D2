55a5fd93e62fa2146393b22967195e662bdef69f
jmc282 <jmc282@pitt.edu>
2018-10-24 16:13:58 -0400

    10/24

--- game_test.rb
+++ game_test.rb
@@ -7,14 +7,30 @@ require_relative 'game'
 
 class GameTest < Minitest::Test
 
+
+    def setup
+      @g = Game::new 10
+    end
+
 	def test_random_int
 	end
 
 	def test_display_starting_message
 	end
 
-	def test_get_units
+	# UNIT TESTS FOR METHOD get_units(amount)
+	# Equivalence classes:
+	# amount = 1 -> returns "1 ounce"
+	# amount != 1 -> returns "#{amount} ounces" 
+	# amount = (not a number)
+	def test_get_units_1
+		assert_equal @g.get_units(1), "1 ounce"
 	end
+
+	def test_get_units_not_1
+		assert_equal @g.get_units(5), "5 ounces"
+	end
+
 
 	def test_convert_currency
 	end
