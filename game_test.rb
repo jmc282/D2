f76976ed54f4afc0664e3fc63e1295f006f68754
jmc282 <jmc282@pitt.edu>
2018-10-24 17:34:12 -0400

    10/24

--- game_test.rb
+++ game_test.rb
@@ -7,11 +7,12 @@ require_relative 'game'
 
 class GameTest < Minitest::Test
 
-
+	# 10 is seed value
     def setup
       @g = Game::new 10
     end
 
+	# UNIT TESTS FOR METHOD 
 	def test_random_int
 	end
 
@@ -20,22 +21,34 @@ class GameTest < Minitest::Test
 
 	# UNIT TESTS FOR METHOD get_units(amount)
 	# Equivalence classes:
-	# amount = 1 -> returns "1 ounce"
+	# amount == 1 -> returns "1 ounce"
 	# amount != 1 -> returns "#{amount} ounces" 
-	# amount = (not a number)
-	def test_get_units_1
+	# amount == (not a number)??
+	def test_get_units_ounce
 		assert_equal @g.get_units(1), "1 ounce"
 	end
 
-	def test_get_units_not_1
+	# UNIT TESTS FOR METHOD 
+	def test_get_units_ounces
 		assert_equal @g.get_units(5), "5 ounces"
 	end
 
-
+	# UNIT TESTS FOR METHOD 
 	def test_convert_currency
 	end
 
-	def test_stop_search?
+	# UNIT TESTS FOR METHOD stop_search?(silver, gold)
+	# Equivalence classes:
+	# silver == 0, gold == 0
+	# silver == 0, gold != 0
+	# silver != 0, gold == 0
+	# silver != 0, gold != 0
+	def test_stop_search_if_none_found
+		assert_equal @g.stop_search?(0, 0), true
+	end
+
+	def test_do_not_stop_search
+		assert_equal @g.stop_search?(1, 0), false
 	end
 
 end
