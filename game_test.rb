35f845eabd5ec12e365285575ebc8ffa623b6b39
Jeff <jfw20@users.noreply.github.com>
2018-10-29 15:10:35 -0400

    Update game_test.rb
    Added some

--- game_test.rb
+++ game_test.rb
@@ -22,24 +22,32 @@ class GameTest < Minitest::Test
 	# amount != 1 -> returns "#{amount} ounces"
 	# amount == (not a number)??
 	def test_get_units_ounce
-		assert_equal @g.get_units(1), "1 ounce"
+		assert_equal @g.get_units(1), '1 ounce'
 	end
 
 	def test_get_units_ounces
-		assert_equal @g.get_units(5), "5 ounces"
+		assert_equal @g.get_units(5), '5 ounces'
 	end
 
   def test_get_units_ounce_less_1
-    assert_raise @g.get_units(-1), "Cannot get less than 1 unit."
+    assert_raise @g.get_units(-1), 'Cannot get less than 1 unit.'
   end
 
-	# UNIT TESTS FOR METHOD
+	# UNIT TESTS FOR METHOD convert_currency(gold, silver)
+  # Equivalence classes:
+  # gold == 1 silver == 0 -> returns "$20.67"
+  # gold == 2 silver == 3 -> returns "45.27"
+  # gold < 0 silver < 0 -> raises 'Currency cannot be negative.'
 	def test_convert_currency
-    assert_equal @g.convert_currency(1, 0), "$1.00"
+    assert_equal @g.convert_currency(1, 0), '$20.67'
 	end
 
+  def test_convert_currency_mixed
+    assert_equal &g.convert_currency(2, 3), '$45.27'
+  end
+
   def test_convert_currency_negative
-    assert_raise @g.convert_currency(-1, -1), NegativeCurrencyError
+    assert_raise @g.convert_currency(-1, -1), 'Currency cannot be negative.'
   end
 
 	# UNIT TESTS FOR METHOD stop_search?(silver, gold)
@@ -52,8 +60,15 @@ class GameTest < Minitest::Test
 		assert_equal @g.stop_search?(0, 0), true
 	end
 
-	def test_do_not_stop_search
-		assert_equal @g.stop_search?(1, 0), false
-	end
+  def test_stop_search_still_gold
+    assert_equal @g.stop_search?(0, 1), false
+  end
 
+  def test_stop_search_still_silver
+    assert_equal @g.stop_search?(1, 0), false
+  end
+
+	def test_stop_search_still_both
+		assert_equal @g.stop_search?(1, 1), false
+	end
 end
