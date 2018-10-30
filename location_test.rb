bac15016928b92cd4bca5206e42a67306046f8e6
Jeff <jfw20@gmail.com>
2018-10-29 23:24:36 -0400

    Created Mock Test Suite
    Test suite for location based tests

---
+++ location_test.rb
@@ -0,0 +1,19 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require_relative 'game'
+require_relative 'location.rb'
+require_relative 'player.rb'
+# Test Suite for player
+class LocationTest < Minitest::Test
+  # 10 is seed value
+  def setup
+    @g = Game.new(10)
+  end
+
+  # UNIT TESTS FOR METHOD method?(args?)
+  # Equivalence classes:
+  # classes go here
+
+
+end
