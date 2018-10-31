989782695c38457362e6957d9d60e8e8cbb6c24d
Jeff <jfw20@users.noreply.github.com>
2018-10-31 12:26:54 -0400

    Changed to take player input

--- player.rb
+++ player.rb
@@ -58,10 +58,10 @@ class Player
   # Returns the minimum amount of metal the miner must find at a location to continue prospecting.
   # If the player is at their first, second, or third site, returns (min gold) 0, and (min silver) 0.
   # If the player is at their fourth or fifth site, returns (min gold) 2, and (min silver) 3.
-  def prospect_min
-    raise 'Cannot have a minimum less than zero.' if @visits < 0
+  def prospect_min(player)
+    raise 'Cannot have a minimum less than zero.' if player.visits < 0
 
-    return [0, 0] if @visits <= 2
+    return [0, 0] if player.visits <= 2
 
     [2, 3]
   end
